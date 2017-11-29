879100f5c0fd28bd6d83a0788de394fa71ab09da
oleg <klemtsov@gmail.com>
2017-11-29 21:26:06 +0300

    Новое приложение

---
+++ src/main/java/controllers/MainController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import com.pi4j.io.gpio.*;
+import com.pi4j.wiringpi.Gpio;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    private final GpioController controller;
+    private final GpioPinDigitalOutput ledPin;
+
+    public MainController(){
+        controller = GpioFactory.getInstance();
+        ledPin = controller.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+    }
+
+    @RequestMapping("/")
+    public String index(){
+        ledPin.toggle();
+
+
+        return "Hallo world!";
+    }
+
+}

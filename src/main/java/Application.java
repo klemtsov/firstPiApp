5b77c66669d43bf573989a236e3a712f2f55e4c6
oleg <klemtsov@gmail.com>
2017-12-01 00:19:49 +0300

    add exot log

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,23 +1,20 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.ContextStoppedEvent;
 
 @SpringBootApplication(scanBasePackages = {"controllers"})
 public class Application {
     public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new ApplicationListener<ContextStoppedEvent>(){
 
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-        context.addApplicationListener(
-                new ApplicationListener<ContextStoppedEvent>() {
-                    @Override
-                    public void onApplicationEvent(ContextStoppedEvent contextStoppedEvent) {
-                        System.out.println(contextStoppedEvent);
-                    }
-                }
-        );
-
+            @Override
+            public void onApplicationEvent(ContextStoppedEvent contextStoppedEvent) {
+                System.out.println(contextStoppedEvent);
+            }
+        });
+        springApplication.run(args);
     }
 
 }

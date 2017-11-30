98817812a242ce31fef27346f87600fdcde8e57a
oleg <klemtsov@gmail.com>
2017-11-30 21:55:32 +0300

    digital indicator

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,40 +1,90 @@
 package controllers;
 
 import com.pi4j.io.gpio.*;
-import com.pi4j.wiringpi.Gpio;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-    private final GpioController controller;
-    private final GpioPinDigitalOutput ledPin;
+    private static final PinState[] off = {
+            PinState.LOW,
+            PinState.LOW,
+            PinState.LOW,
+            PinState.LOW,
+            PinState.LOW,
+            PinState.LOW,
+            PinState.LOW};
+    private static final PinState[] one = {
+            PinState.LOW,
+            PinState.LOW,
+            PinState.HIGH,
+            PinState.HIGH,
+            PinState.LOW,
+            PinState.LOW,
+            PinState.LOW};
 
-    public MainController(){
+    private static final PinState[] two = {
+            PinState.HIGH,
+            PinState.HIGH,
+            PinState.LOW,
+            PinState.HIGH,
+            PinState.HIGH,
+            PinState.HIGH,
+            PinState.LOW};
+
+    private final GpioController controller;
+
+    private final GpioPinDigitalOutput[] pinDigitalOutputs;
+
+
+    public MainController() {
         controller = GpioFactory.getInstance();
-        ledPin = controller.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+        this.pinDigitalOutputs = new GpioPinDigitalOutput[]{
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_01, "11", PinState.LOW),
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_02, "7", PinState.LOW),
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_03, "4", PinState.LOW),
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_04, "2", PinState.LOW),
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_05, "1", PinState.LOW),
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_06, "5", PinState.LOW),
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_07, "10", PinState.LOW),
+        };
+
     }
 
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         //ledPin.toggle();
         //System.out.println("включен");
         return "Hallo world!";
     }
 
-    @RequestMapping("/ledon")
-    public String ledon(){
-        ledPin.high();
-        System.out.println("включен");
-        return "включен";
+
+    private void digitsChange(final PinState[] pinStates) {
+        int index = 0;
+        for (GpioPinDigitalOutput pin :
+                pinDigitalOutputs) {
+            pin.setState(pinStates[index]);
+            index++;
+        }
     }
 
-    @RequestMapping("/ledoff")
-    public String ledoff(){
-        ledPin.low();
-        System.out.println("включен");
-        return "включен";
+    @GetMapping("/dig/{digit}")
+    public String switchDigit(@PathVariable int digit) {
+        switch (digit) {
+            case -1:digitsChange(off);
+            case 1:digitsChange(one);
+                break;
+            case 2:digitsChange(two);
+                break;
+            default:
+                break;
+        }
+        String mess = String.format("цифра %d", digit);
+        System.out.println(mess);
+        return mess;
     }
 
 }
